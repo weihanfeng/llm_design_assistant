34b114af5e0b25e465809d225aa402d44b9bbfc3
whfeng <weihan.feng.contact@gmail.com>
2024-03-01 11:38:00 +0800

    Add streamlit

--- multi-agent-examples/main.py
+++ multi-agent-examples/main.py
@@ -3,6 +3,8 @@ from textwrap import dedent
 from archi_agents import Architecture_idea_exploration_agent
 from langchain_community.chat_models import ChatOpenAI
 from utils import extract_and_parse_list_of_dicts, generate_image_from_prompts, convert_image, generate_image_mask
+from  archi_tasks import task1, task2, generate_task_with_brief, task4
+from archi_crew import ArchitectureDesignCrew
 
 import os
 from dotenv import load_dotenv, find_dotenv
@@ -23,68 +25,11 @@ The community center should be designed to be accessible to all residents, inclu
 The community center should be designed to be flexible and adaptable to different uses.
 """)
 
-task1 = dedent(f"""
-Given the design brief below, try to understand the task and generate questions to research. 
-The questions should be focused on understanding the actual content of the brief, the site, the people, the culture, the surrounding environment, climate and so on.
-The answer to the questions should be readily searchable on the internet.
-               
-The brief: {brief}
-""")
+tasks = [task1, task2, generate_task_with_brief(brief), task4]
 
-task2 = dedent(f"""
-Given the questions, research the answers to the questions and summarize the findings.
-You must always use the tool to search the internet to find the answers to the questions.
-
-After you have obtained the findings, output the summary incorporating all the research findings. Your final answer is the summary.         
-""")
-
-task3 = dedent(f"""
-Given the design brief and the research findings, generate 5 vastly different architecture design concepts which are unique, interesting, innovative and meets the requirements of the brief.
-The design concepts should make use of research findings.
-The design concepts should be focus on describing the actual form of the design, to help with visualization.
-
-The brief:
-{brief}
-               """)
-
-task4 = dedent(f"""
-Given the design concepts, generate a positive prompt and a negative prompt for each concept, the prompts will be the inputs to a text to image model to generate images of the concepts.
-The prompts should be brief and focused on describing the actual form of the design, to help with visualization.
-Give your output as a list of json objects in the format: 
-{{"concept": "name of concept", "positive": "positive prompt", "negative": "negative prompt"}}."""
-)
-               
-tasks = [task1, task2, task3, task4]
-class ArchitectureDesignCrew:
-
-  def __init__(self, tasks):
-    self.tasks = tasks
-
-  def run(self):
-    agents = Architecture_idea_exploration_agent(llm=llm)
-    tasks = self.tasks
-    brief_understanding_agent = agents.architecture_brief_question_agent()
-    research_assistant_agent = agents.research_assistant()
-    concept_generation_agent = agents.concept_generation_agent()
-    text_to_image_prompt_agent = agents.text_to_image_prompt_agent()
-    agents = [brief_understanding_agent, research_assistant_agent, concept_generation_agent, text_to_image_prompt_agent]
-    task1 = Task(description=tasks[0], agent=brief_understanding_agent)
-    task2 = Task(description=tasks[1], agent=research_assistant_agent)
-    task3 = Task(description=tasks[2], agent=concept_generation_agent)
-    task4 = Task(description=tasks[3], agent=concept_generation_agent)
-    tasks = [task1, task2, task3, task4]
-    
-    crew = Crew(
-      agents=agents,
-      tasks=tasks,
-      verbose=True
-    )
-
-    result = crew.kickoff()
-    return result
 
 if __name__ == "__main__":
-  design_crew = ArchitectureDesignCrew(tasks)
+  design_crew = ArchitectureDesignCrew(tasks, llm)
   result = design_crew.run()
   # print(result)
   
